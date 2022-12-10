21ab313572dea77b6c8bbf865f311c94dc2d2e60
Ivo Biesdorf <ivobiesdorf@gmail.com>
2022-12-10 12:51:50 -0300

    Dois

--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -6,26 +6,26 @@ import { Card } from '../../components/Card';
 export const Posts = ()=>{
     const [photos, setPhotos] = useState([])
     const [album, setAlbum] = useState([])
-    
+    const [loading, setLoading] = useState(true)
+
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/photos') 
         .then(response => response.json())
         .then(json => {
             setPhotos(json)
+            setLoading(false)
+            
         })
-    },[]);
-    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/albums') 
         .then(response => response.json())
         .then(json => {
             setAlbum(json)
         })
     },[]);
-   
     
     return (
         <div className='corpo-posts'>
-            {photos && photos.map((a, i)=>{
+            {loading? <div className='loading'><i className="fa fa-spinner fa-pulse fa-5x fa-fw" aria-hidden="true"></i></div>: photos.map((a, i)=>{
                 return( 
                     // <Card url={a.url}> {a.title} </Card>
                     <Card props={a} album={album[a.albumId]}/> 
